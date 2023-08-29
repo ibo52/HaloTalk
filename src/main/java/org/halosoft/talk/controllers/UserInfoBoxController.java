a16bc2dce51de10e4eb9920ac44f8777a95c7026
ibo52 <halilibrahimmut@hotmail.com>
2023-08-29 16:31:45 +0300

    update components to show user info

--- src/main/java/org/halosoft/talk/controllers/UserInfoBoxController.java
+++ src/main/java/org/halosoft/talk/controllers/UserInfoBoxController.java
@@ -10,14 +10,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 /**
  * FXML Controller class
  *
  * @author ibrahim
  */
 public class UserInfoBoxController implements Initializable {
-
+    
+    private String id;
 
     @FXML
     private ImageView userImage;
@@ -27,16 +32,67 @@ public class UserInfoBoxController implements Initializable {
     private Label lastSeenLabel;
     @FXML
     private Label unreadMessageCountLabel;
+    @FXML
+    private VBox userImageBox;
+    @FXML
+    private Label customStatusLabel;
+    @FXML
+    private BorderPane rootPane;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        setStatus(0);
         // TODO
     }   
     
     public void setUserName(String name){
         this.userID.setText(name);
+    }
+    
+    public void setStatus(int status){
+        String statusStyle="-fx-background-color: %s; -fx-background-radius: 30% 30%;";
+        
+        Tooltip ttip=new Tooltip();
+        
+        switch(status){
+            case 0:
+                this.userImageBox.setStyle(statusStyle.replace("%s","gray"));
+                ttip.setText("Offline");
+                break;
+                
+            case 1:
+                this.userImageBox.setStyle(statusStyle.replace("%s","red"));
+                ttip.setText("Busy");
+                break;
+                
+            case 2:
+                this.userImageBox.setStyle(statusStyle.replace("%s","green"));
+                ttip.setText("Online");
+                break;
+        }
+        
+        Tooltip.install(this.rootPane, ttip);
+        //Tooltip.install(this.userImageBox, ttip);
+        
+        
+    }
+    
+    public void setCustomStatus(String status){
+        customStatusLabel.setText(status);
+    }
+    
+    public String getID(){
+        return this.id;
+    }
+    public void setID(String id){
+        this.id=id;
+    }
+
+    @FXML
+    private void rootPaneMouseClicked(MouseEvent event) {
+        System.out.println("clicked");
     }
     
 }
