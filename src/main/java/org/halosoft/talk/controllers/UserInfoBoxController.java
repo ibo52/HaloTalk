e43fef690827a1a45bc49a1090568ed28cc17d09
ibo52 <halilibrahimmut@hotmail.com>
2023-09-01 22:54:20 +0300

    Fix resize: bind imageDetail panel size to its parents

--- src/main/java/org/halosoft/talk/controllers/UserInfoBoxController.java
+++ src/main/java/org/halosoft/talk/controllers/UserInfoBoxController.java
@@ -7,8 +7,8 @@ package org.halosoft.talk.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 
@@ -18,7 +18,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import org.halosoft.talk.App;
 /**
  * FXML Controller class
@@ -49,6 +51,7 @@ public class UserInfoBoxController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setStatus(0);
+        setID("user");
         // TODO
     }   
     
@@ -100,16 +103,59 @@ public class UserInfoBoxController implements Initializable {
     public Image getImage(){
         return this.userImage.getImage();
     }
+    
+    private void showUserInfoDetails(){
+        
+        try {
+            Parent imageDetails=App.loadFXML("imageDetails");
+            ImageDetailsController imageDetailsController=(ImageDetailsController) imageDetails.getUserData();
+            
+            //get hostSelector rootPane and its controls
+            Parent p=this.rootPane.getParent().getParent().getParent().getParent().getParent().getParent();
+            HostSelectorController ctrlr=(HostSelectorController) p.getUserData();
+            
+            //set imageView and add to left stackpane
+            StackPane leftStackPane=ctrlr.getLeftStackPane();
+            
+            //set max size of child at parent
+            imageDetailsController.initRootProperty(leftStackPane.widthProperty().multiply(0.8));
+            
+            imageDetailsController.setUserID(this.getUserID());
+            //imageDetailsController.setUserImage(this.getImage());
+            leftStackPane.getChildren().add(imageDetails);
+            
+            //bind width and height to parent
+            
+            //scale transition for fancy effect
+            ScaleTransition st=new ScaleTransition();
+            st.setNode(imageDetails);
+            st.setDuration(Duration.millis(900));
+            
+            st.setFromX(0);
+            st.setFromY(0);
+            st.setToX(1);
+            st.setToY(1);
+            st.play();
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
     @FXML
     private void rootPaneMouseClicked(MouseEvent event) {
         
         //get hostselector rootpane and its controller
-        Parent p=this.rootPane.getParent().getParent().getParent().getParent().getParent();
+        Parent p=this.rootPane.getParent().getParent().getParent().getParent().getParent().getParent();
         
         HostSelectorController ctrlr=(HostSelectorController) p.getUserData();
 
         ctrlr.bringChatScreen((UserInfoBoxController) this.rootPane.getUserData());
+    }
+
+    @FXML
+    private void userImageBoxMouseClicked(MouseEvent event) {
+        this.showUserInfoDetails();
     }
     
 }
