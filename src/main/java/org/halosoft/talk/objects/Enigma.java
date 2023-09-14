3b2d10773feff2347837e75131ef6ac1c9bbcfa9
ibo52 <halilibrahimmut@hotmail.com>
2023-09-14 23:17:23 +0300

    Revert "add general utility to encrypt/decrypt messages"
    This reverts commit 1b7cc205a0dcb79348b5d66d2828ff3bf015337f.

--- src/main/java/org/halosoft/talk/objects/Enigma.java
+++ src/main/java/org/halosoft/talk/objects/Enigma.java
@@ -4,8 +4,6 @@
  */
 package org.halosoft.talk.objects;
 
-import java.math.BigInteger;
-
 /**
  *
  * @author ibrahim
@@ -14,85 +12,34 @@ import java.math.BigInteger;
  */
 public class Enigma {
     
-    public static String encrypt(int[] data, long[] key){
-        /*SUM(data^e mod n) formula ciphers the ascii message*/
-        
-        StringBuilder ciphered=new StringBuilder("");
-
-        for(int i=0; i<data.length; i++){
-
-            BigInteger bi=new BigInteger( Integer.toString(data[i] ) );
-
-            bi=bi.modPow(new BigInteger( Long.toString( key[1] ) ),
-                    new BigInteger( Long.toString( key[0] ) ) );
-            
-
-            ciphered.append(DataManipulator.BigInterToHex(bi) );
-        }
-        return ciphered.toString();
+    public Enigma(){
         
     }
     
-    public static String decrypt(int[] data, long[] key){
+    public static byte[] encrypt(byte[] data, int[] key){
+        /*SUM(data^e mod n) formula ciphers the ascii message*/
+        
+        byte[] ciphered=new byte[data.length];
+        
+        for(int i=0; i<data.length; i++){
+            ciphered[i]=(byte) (Math.pow( data[i] , key[1]) % key[0]);
+        }
+        
+        return ciphered;
+        
+    }
+    
+    public static byte[] decrypt(byte[] data, int[] key){
         /*as c represents encrypted_message: c^d mod n formula
         decyphers the message by ascii table*/
         
-        return Enigma.encrypt(data, key);
+        byte[] deciphered=new byte[data.length];
         
-    }
-    
-    public static String encrypt(String textData, long[] key){
-        /*SUM(data^e mod n) formula ciphers the ascii message*/
-        //Encrypts general text message to special hex as (\datalen+data)
-        StringBuilder ciphered=new StringBuilder("");
-        
-        for(int i=0; i<textData.length(); i++){
-
-            BigInteger bi=new BigInteger( 
-                    textData.substring(i, i+1).getBytes() );
-
-            bi=bi.modPow(new BigInteger( Long.toString( key[1] ) ),
-                    new BigInteger( Long.toString( key[0] ) ) );
-
-            ciphered.append(DataManipulator.BigInterToHex(bi) );
-        }
-
-        return ciphered.toString();
-        
-    }
-    public static String decrypt(String DataManipulatorHexData, long[] key){
-        //Decrypts special hex message to special to string
-        StringBuilder deciphered=new StringBuilder("");
-        String[] data=DataManipulator.parseHex(DataManipulatorHexData);
-        
-        for(String d:data){
-            
-            BigInteger bi=new BigInteger( d, 16 );
-            System.out.println(d+" to "+bi);
-            bi=bi.modPow(new BigInteger( Long.toString( key[1] ) ),
-                    new BigInteger( Long.toString( key[0] ) ) );
-            
-            deciphered.append(bi.toString(16));
-        }
-        return deciphered.toString();
-    }
-    
-    public static String encrypt(byte[] data, long[] key){
-        /*SUM(data^e mod n) formula ciphers the ascii message*/
-        
-        StringBuilder ciphered=new StringBuilder("");
-
         for(int i=0; i<data.length; i++){
-
-            BigInteger bi=new BigInteger( Integer.toString(data[i] ) );
-
-            bi=bi.modPow(new BigInteger( Long.toString( key[1] ) ),
-                    new BigInteger( Long.toString( key[0] ) ) );
-            
-
-            ciphered.append(DataManipulator.BigInterToHex(bi) );
+            deciphered[i]=(byte) (Math.pow( data[i] , key[1]) % key[0]);
         }
-        return ciphered.toString();
+        
+        return deciphered;
         
     }
 }
