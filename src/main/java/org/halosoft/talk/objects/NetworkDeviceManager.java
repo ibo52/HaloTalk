a4994eff159375948211b231d5f677e14a531f37
ibo52 <halilibrahimmut@hotmail.com>
2023-09-17 12:35:16 +0300

    add method to check connectivity of machine

--- src/main/java/org/halosoft/talk/objects/NetworkDeviceManager.java
+++ src/main/java/org/halosoft/talk/objects/NetworkDeviceManager.java
@@ -8,6 +8,7 @@ import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,6 +17,9 @@ import java.util.HashMap;
 /**
  *
  * @author ibrahim
+ * Manager for network interfaces on machine.
+ * Provides list of those parsed interfaces according
+ * to wanted connection type(Wireless, wired, loopback) of network.
  */
 public class NetworkDeviceManager {
     
@@ -62,11 +66,29 @@ public class NetworkDeviceManager {
                     }
                 }
             }
+            
+            //if no wireless device, replace arraylist with loopbacks one
+            if (  this.devices.get(this.ConnectionTypeKeyWords[0]).isEmpty()  ) {
+                this.devices.replace(this.ConnectionTypeKeyWords[0]
+                        ,this.devices.get(this.ConnectionTypeKeyWords[2]));
+            }
+            //if no ethernet device, replace arraylist with loopbacks one
+            if (  this.devices.get(this.ConnectionTypeKeyWords[1]).isEmpty()  ) {
+                this.devices.replace(this.ConnectionTypeKeyWords[1]
+                        ,this.devices.get(this.ConnectionTypeKeyWords[2]));
+            }
+            
         } catch (SocketException ex) {
             System.out.println(this.getClass().getName()+":"+ex.getMessage());
         }
     }
     
+    /**
+     * Get network interfaces on machine of specified type
+     * @param connectionType specific type of connection that interface operates
+     * @return desired type of devices list if machine connected to internet by
+     * these devices, or else returns ArrayList of size 1 which contains loopback
+     */
     public ArrayList<NetworkInterface> getInterfaceDevices(ConnectionType connectionType){
 
         switch(connectionType){
@@ -84,10 +106,21 @@ public class NetworkDeviceManager {
                 return null;
         }
     }
-
+    
+    /**
+     * Calculates network identity by address of given network interface.
+     * @param ni Desired type of interface device
+     * @return network identity if machine is connected to internet, 
+     * or returns loopback identity(127.0.0.0 generally)
+     */
     public static String calculateNetworkIdentity(NetworkInterface ni){
        /*Returns network identity of sub network*/
         InetAddress local=null;
+        try {
+            local = InetAddress.getLocalHost();
+        } catch (UnknownHostException ex) {
+            System.out.println(ex.getMessage());
+        }
         int subnetMask=-1;
 
         for (InterfaceAddress addr:ni.getInterfaceAddresses()){
@@ -111,5 +144,29 @@ public class NetworkDeviceManager {
                    +Byte.toUnsignedInt(calc[3]);
        //return calculated identity address
        return host;
+    }
+    
+    /**
+     * checks for connectivity by looking network interfaces list.
+     * If there is 1 interface in list, which is loopback, that means you
+     * simply do not have an interface that connected to internet.
+     * @return true if there is internet connection, else false
+     */
+    public static boolean checkForConnectivity(){
+
+        try {
+
+            ArrayList<NetworkInterface> interfaces = Collections.list(
+                    NetworkInterface.getNetworkInterfaces());
+            
+            if ( interfaces.size()>1 ) {
+                return true;
+            }
+            return false;
+            
+        } catch (SocketException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 }
