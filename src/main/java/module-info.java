ec0f7d6bf90c104d2585cb192b0c9e13551a94c7
ibo52 <halilibrahimmut@hotmail.com>
2023-09-29 18:04:34 +0300

    add 'require java.logging' to log exceptions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.halosoft.talk {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.logging;
 
     opens org.halosoft.talk.controllers to javafx.fxml;
     opens org.halosoft.talk to javafx.fxml;
