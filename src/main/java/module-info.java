f3512049ac5573cb4a0cfec5f5e864290f1ddee1
ibo52 <halilibrahimmut@hotmail.com>
2023-10-01 15:53:27 +0300

    give permission for javafx to access setting package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.halosoft.talk {
     requires java.logging;
 
     opens org.halosoft.talk.controllers to javafx.fxml;
+    opens org.halosoft.talk.controllers.setting to javafx.fxml;
     opens org.halosoft.talk to javafx.fxml;
     exports org.halosoft.talk;
 }
