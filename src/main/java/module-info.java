6147a831d1615fa2379a7b7f24c11d9fb7b91286
ibo52 <halilibrahimmut@hotmail.com>
2023-10-02 23:39:28 +0300

    open adapters package to javafx.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,6 @@ module org.halosoft.talk {
     opens org.halosoft.talk.controllers to javafx.fxml;
     opens org.halosoft.talk.controllers.setting to javafx.fxml;
     opens org.halosoft.talk to javafx.fxml;
+    opens org.halosoft.talk.adapters to javafx.fxml;
     exports org.halosoft.talk;
 }
