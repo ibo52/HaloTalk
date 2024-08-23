297c250fec9f80503faa9f1e6fbbb79f6c1e8c4e
ibo52 <halilibrahimmut@gmail.com>
2024-08-23 23:56:19 +0300

    change project to multi module maven project

---
+++ src/main/java/org/halosoft/Main.java
@@ -0,0 +1,12 @@
+package org.halosoft.talk;
+//import org.halosoft.gui.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //launch javafx app
+        //App.main(args);
+        
+    }
+}
