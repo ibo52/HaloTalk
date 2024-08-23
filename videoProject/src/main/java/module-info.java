37528bb9f52f595ff6fc4f79c6f54edbb402afe0
ibo52 <halilibrahimmut@gmail.com>
2024-08-23 23:58:04 +0300

    remove old and junk

--- videoProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.ibo52 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.ibo52 to javafx.fxml;
-    opens org.ibo52.controllers to javafx.fxml;
-    exports org.ibo52;
-}
