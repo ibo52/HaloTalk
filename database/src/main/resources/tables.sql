61d65d6d76325552a9e405095059b29622269ef4
ibo52 <halilibrahimmut@gmail.com>
2024-08-29 23:30:18 +0300

    sql script to build and keep chat history on sqlite db

---
+++ database/src/main/resources/tables.sql
@@ -0,0 +1,23 @@
+--creates database tables to log messaging between local and remote
+create table Sender
+(
+    id integer not null primary key,
+    ip text not null
+);
+
+--message queue between current device and remote
+create table MessageQueue
+(   id integer not null primary key,
+    senderId text not null default 0,
+    message text not null,
+    gmtdate date not null default current_date,
+    gmttime time not null default current_time,
+    completed integer not null default 1,
+    --direction integer not null default 0,
+
+    foreign key(senderId) references Sender(id)
+);
+
+--localhost information is mandatory as id of table Sender defaults to 0
+insert into sender(id, ip) values(0, 'localhost');
+
